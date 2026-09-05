4ccceba692df294008913f24cfca4c6760775013
GitPirates <starshivamverma2@gmail.com>
2026-09-05 22:49:57 +0530

    feat: configure Vitest with jsdom environment and dotenv support

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vitest/config";
 import path from "path";
+import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        env: process.env,
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
